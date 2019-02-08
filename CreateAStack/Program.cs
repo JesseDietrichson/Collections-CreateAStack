66766b5220e5bcca22491f86bd335dcbc4c738a2
Jesse Dietrichson <jesse.dietrichson@xamarin.com>
2019-02-08 02:51:02 -0500

    Add project files.

---
+++ CreateAStack/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateAStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> myStack = new Stack<int>();
+
+            //the stack is empty
+
+            myStack.Push(1);
+            myStack.Push(2);
+            myStack.Push(3);
+            myStack.Push(4);
+
+            Console.WriteLine(myStack.Peek());
+
+            int topNumber = myStack.Pop();
+
+            Console.WriteLine(myStack.Peek());
+
+        }
+    }
+}
